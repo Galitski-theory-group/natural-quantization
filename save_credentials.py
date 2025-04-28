f1568e84a6ce3a477fff9a6c3ff7255887db84f5
Djamil17 <dl2774@columbia.edu>
2025-04-28 17:24:15 -0400

    feat: records data and collects results per shot

--- save_credentials.py
+++ save_credentials.py
@@ -5,6 +5,8 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 
 if len(sys.argv) > 1:
     token = sys.argv[1]
-    QiskitRuntimeService.save_account(token=token, channel="ibm_quantum", overwrite=True)
+    QiskitRuntimeService.save_account(
+        token=token, channel="ibm_quantum", overwrite=True
+    )
 else:
     print("Please provide token")
